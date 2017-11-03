3844b78894f548f8c38225344b4f826151311db8
Daniel <003wuthnowd@gmail.com>
2017-11-03 16:47:27 -0500

    added the counter and made changes to the form test

---
+++ Daniel_Wuthnow/python_flask/flask_fundamentals/counter/server.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request, redirect, session
+app = Flask(__name__)
+app.secret_key = 'ThisIsSecret'
+
+#adds one on refresh
+@app.route('/')
+def welcome():
+	if 'counter' not in session :
+		session['counter']=0
+	else:
+		session['counter']+=1
+	return render_template('index.html')
+
+#adds 1 from the refresh and one from the button
+@app.route('/addtwo', methods=['POST'])
+def addtwo():
+	if 'counter' not in session :
+		session['counter']=0
+	else:
+		session['counter']+=1
+	return redirect('/')
+
+
+
+#clears the session
+@app.route('/clear', methods=['POST'])
+def clear():
+	session.clear()
+	return redirect('/')
+# @app.route('/reset')
+# def reset():
+# 	session['counter'] = 0
+# 	return redirect('/') 
+
+
+app.run(debug=True)
