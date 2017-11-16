f2d45750296d0f64a18d3518cdfba86617505a14
Daniel <003wuthnowd@gmail.com>
2017-11-16 15:23:15 -0600

    adding django_app'

---
+++ Daniel_Wuthnow/python_django/django_app/apps/first_app/views.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render, HttpResponse, redirect
+
+# Create your views here.
+def index(request):
+	response = "Plasceholder to later display all the list of blogs"
+	return HttpResponse(response)
+
+def new(request):
+	response = "This is a Plasceholder to display a new form to create a new blog"
+	return HttpResponse(response)
+
+def create(request):
+	return redirect('/')
+
+def show(request, number):
+	response = "Plasceholder for blog "+number
+	return HttpResponse(response)
+
+def edit(request, number):
+	response = "Plasceholder to edit blog "+number
+	return HttpResponse(response)
+
+def destroy(request, number):
+	return redirect('/')
