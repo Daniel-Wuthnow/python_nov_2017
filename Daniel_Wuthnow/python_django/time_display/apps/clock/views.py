a7f1e3ec353e806d729fec7ca7aa7b2b2b9079d1
Daniel <003wuthnowd@gmail.com>
2017-11-16 16:31:50 -0600

    correct datetime

--- Daniel_Wuthnow/python_django/time_display/apps/clock/views.py
+++ Daniel_Wuthnow/python_django/time_display/apps/clock/views.py
@@ -3,10 +3,11 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render, HttpResponse, redirect
 from time import gmtime, strftime
+from datetime import datetime
 
 # Create your views here.
 def index(request):
 	contex = {
-		"time" : strftime("%b %d, %Y %H:%M %p", gmtime())
+		"time" : datetime.now()
 	}
 	return render(request, "clock/index.html", contex)
